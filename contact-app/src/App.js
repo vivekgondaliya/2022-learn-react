755ce84e0cf1f7a6863cae27969be298093958e5
Vivek Gondalia <vivekgondaliya@gmail.com>
2022-08-18 16:28:50 +0530

    useEffect Hook : setup complete, does not persist data

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -23,9 +23,15 @@ function App() {
 	const [contacts, setContacts] = useState([]);
 	
 	const addContactHandler = (contact) => {
-		console.log(contact);
 		setContacts([...contacts, contact]);
 	}
+
+	useEffect(() => {
+		const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+		if(retrieveContacts) 
+			setContacts(retrieveContacts);
+
+	}, []);
 
 	useEffect(() => {
 		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
